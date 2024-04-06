493b351000d21d380401717b77a8c8ae8862725a
tnalli417 <tnalli417@student.fuhsd.org>
2024-04-06 16:56:03 -0700

    Built in requests

--- src/app.py
+++ src/app.py
@@ -1,37 +1,21 @@
 from flask import Flask, render_template
 import requests
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return render_template("index.html")
-
-
-@app.route('/reddit_api/<username>', methods = ["GET", "POST"])
-def reddit_api(username):
-    return "YEow!"
-
-
-@app.route("/sportsnews", methods = ["GET", "POST"])
-def current_scores():
+@app.route('/sportsnews')
+def sports_news():
     response = requests.get(
-        'https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=c3b58e07353b4680873794649725a0ba')
+        'https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=YOUR_API_KEY')
     data = response.json()
 
-    top_titles = []
-    top_urls = []
+    articles = []
 
     for article in data['articles'][:5]:
-        top_titles.append(article['title'])
-        top_urls.append(article['url'])
+        articles.append({'title': article['title'], 'url': article['url']})
+    return data
 
-    result = '\n'.join([f"{title}\n{url}" for title, url in zip(top_titles, top_urls)])
-
-    return result
-
-
-@app.route
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
