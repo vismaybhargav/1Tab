42c2bc0cd34b6b49ee5100d0a6cf7446042d6beb
tnalli417 <tnalli417@student.fuhsd.org>
2024-04-06 21:24:31 -0700

    Removed unnecessary comments

--- src/js/feed-component.js
+++ src/js/feed-component.js
@@ -9,23 +9,22 @@ async function createColumn(source) {
     const columnContent = document.createElement('div');
     columnContent.className = 'column-content';
 
-    // Fetch data based on the selected source
     try {
         if (source === 'Sports News') {
             const response = await fetch('https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/sportsnews');
-            columnContent.innerHTML = await response.text(); // Set the content of the column to the fetched data
+            columnContent.innerHTML = await response.text();
         }
         if (source === 'Github Trending') {
             const response = await fetch('https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/git_trending');
-            columnContent.innerHTML = await response.text(); // Set the content of the column to the fetched data
+            columnContent.innerHTML = await response.text();
         }
         if (source === 'The New York Times') {
             const response = await fetch('https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/nytimes');
-            columnContent.innerHTML = await response.text(); // Set the content of the column to the fetched data
+            columnContent.innerHTML = await response.text();
         }
         if (source === 'Stocks') {
             const response = await fetch('https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/stocks');
-            columnContent.innerHTML = await response.text(); // Set the content of the column to the fetched data
+            columnContent.innerHTML = await response.text();
         }
     } catch (error) {
         console.error('Error fetching response:', error);
