57d9a497d48f05e7fe25c44332e1082327a70878
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-04-06 21:16:26 -0700

    refactored user-config pending tests

--- src/js/user-config.js
+++ src/js/user-config.js
@@ -1,27 +1,27 @@
 let idCount = 0;
 
-/**
- * Generates UserData json object and places it into the localStorage for future use. <br><b>
+/** * Generates UserData json object and places it into the localStorage for future use. <br><b>
  * Make sure that the userData hasn't already been created by this point
- * @param name name of the user
+ * @param id id of the user
  * @param subscribedFeeds a string array of the feeds that they have subscribed to
  */
-function createUserData(name, subscribedFeeds) {
+function createUserData(id, subscribedFeeds) {
     let userData = {
-        "name": name,
+        "id": id,
         "subscribed-feeds": subscribedFeeds
     }
-    localStorage.setItem("onetab-userdata", JSON.stringify(userData));
+    let userString = JSON.stringify(userData);
+    localStorage.setItem("onetab-userdata", userString);
+    idCount++;
+    return userData;
 }
 
 /**
  * Gets the user data from the local storage if it is available, else generates it.a
  */
 function getUserData() {
-    if (localStorage.getItem("onetab-userdata") != null) {
-        return JSON.parse(localStorage.getItem("onetab-userdata"));
-    } else {
-        createUserData(idCount, []);
-        idCount++;
-    }
+    let user = JSON.parse(localStorage.getItem("onetab-userdata") == null
+        ? createUserData(idCount, []) : localStorage.getItem("onetab-userdata"));
+    console.log(user);
+    return user;
 }
