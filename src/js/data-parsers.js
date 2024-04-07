ff8abb94ec7934d34f24b745c1d89c618bffa10f
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-04-06 22:32:40 -0700

    temp fix for clickable divs

--- src/js/data-parsers.js
+++ src/js/data-parsers.js
@@ -3,10 +3,12 @@ function parseGithub(jsonString, columnContent) {
 
     for(let i = 0; i < columnData.length; i++) {
         columnContent.innerHTML += `
+        <a href="${columnData[i].url}">
             <div class="column-block">
-                <h1>${columnData[i].username}/${columnData[i].repo.name}</h1>
-                <h3>${columnData[i].repo.description}</h3>
+                <h3>${columnData[i].username}/${columnData[i].repo.name}</h3>
+                <h5>${columnData[i].repo.description}</h5>
             </div>
+        </a>
         `;
     }
 }
