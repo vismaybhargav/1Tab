70af12ea967cb5d06f35ffe623f15106cdb9a32c
tnalli417 <tnalli417@student.fuhsd.org>
2024-04-07 01:23:38 -0700

    Made Dropdown Menu Look Better

--- src/js/feed-component.js
+++ src/js/feed-component.js
@@ -14,18 +14,18 @@ async function createColumn(source) {
         switch (source) {
             case "Sports News":
                 uri = "https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/sportsnews";
+                columnHeader.className = 'column-header-sports-news';
                 break;
             case "Github Trending":
+                columnHeader.className = 'column-header-github';
                 uri = "https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/git_trending";
                 break;
             case "The New York Times":
                 uri = "https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/nytimes";
+                columnHeader.className = 'column-header-new-york-times';
                 break;
             case "Stocks":
                 uri = "https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/stocks";
-                break;
-            case "Hacker News":
-                uri = "https://d4f7d3de-971a-4441-bcef-425aec930868-00-tc400qk9kwau.janeway.replit.dev/hackernews";
                 break;
         }
 
